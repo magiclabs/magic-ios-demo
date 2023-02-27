5799ca9ba8449e801b1b3bd433dca86ec05bb214
Arian Flores <arian.flores@magic.link>
2023-02-27 11:13:32 -0800

    Adds MC Methods

--- Shared-ui/magic-ios-demo/Main/MagicViewController.swift
+++ Shared-ui/magic-ios-demo/Main/MagicViewController.swift
@@ -84,6 +84,50 @@ class MagicViewController: UIViewController {
             })
     }
     
+    // MARK: - Magic Connect Methods
+    @IBAction func showUI() {
+        guard let magic = magic else { return }
+        magic.wallet.showUI(response: { response in
+                if (response.error != nil) {
+                    print(response.error.debugDescription)
+                }
+                print(response.result?.description ?? "")
+            })
+    }
+
+    @IBAction func getInfo() {
+        guard let magic = magic else { return }
+        magic.wallet.getInfo(response: { response in
+            if (response.error != nil) {
+                print(response.error.debugDescription)
+            }
+            self.showResult("Wallet Type: \(response.result?.walletType ?? "No Wallet Type Found")")
+        })
+    }
+
+
+    @IBAction func requestUserInfoWithUI() {
+        guard let magic = magic else { return }
+        magic.wallet.requestUserInfoWithUI(response: { response in
+            if (response.error != nil) {
+                print(response.error.debugDescription)
+            }
+            self.showResult("Email: \(response.result?.email ?? "No Email Found")")
+       })
+    }
+    
+    @IBAction func disconnect() {
+        guard let magic = magic else { return }
+        
+        magic.wallet.disconnect(response: { response in
+            print(response.result?.description ?? "")
+            UserDefaults.standard.removeObject(forKey: "publicAddress")
+            self.navigateToLogin()
+        })
+    }
+
+    
+    // MARK: - Magic Auth Methods
     @IBAction func logOut() {
         guard let magic = magic else { return }
         magic.user.logout(response: { response in
